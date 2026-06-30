2ad9b1457edbd77de8a52fc8595de0470b425008
mrifki26 <mrifki260405@gmail.com>
2026-07-01 03:47:36 +0700

    Polish app logo presentation

--- chili_track/lib/widgets/app_logo.dart
+++ chili_track/lib/widgets/app_logo.dart
@@ -8,20 +8,35 @@ class AppLogo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       width: size,
       height: size,
-      child: Padding(
-        padding: EdgeInsets.all(padding),
-        child: Image.asset(
-          "assets/logo/image.png",
-          fit: BoxFit.contain,
-          errorBuilder: (context, error, stackTrace) {
-            return const Icon(
-              Icons.local_florist_rounded,
-              color: Color(0xFFC91524),
-            );
-          },
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+        border: Border.all(color: const Color(0xFFE9D8DC), width: 1),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x12000000),
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: ClipOval(
+        child: Transform.scale(
+          scale: 1.18,
+          child: Image.asset(
+            "assets/logo/image.png",
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return const Icon(
+                Icons.local_florist_rounded,
+                color: Color(0xFFC91524),
+              );
+            },
+          ),
         ),
       ),
     );
